1d20318910f64766c8c1e955916ce78e6ff23d1f
Jussi <jussi@lingvi.st>
2018-03-24 17:51:27 -0700

    hep

--- confusionmatrix.py
+++ confusionmatrix.py
@@ -1,1 +1,36 @@
-../utils/confusionmatrix.py
+class ConfusionMatrix:
+    '''ConfusionMatrix keeps book over a categorisation excercise, most notably its errors.'''
+    def __init__(self):
+        self.confusionmatrix = {}
+
+    def addconfusion(self, rfacit, rpredicted):
+        facit = str(rfacit)
+        predicted = str(rpredicted)
+        if facit in self.confusionmatrix:
+            if predicted in self.confusionmatrix[facit]:
+                self.confusionmatrix[facit][predicted] += 1
+            else:
+                self.confusionmatrix[facit][predicted] = 1
+        else:
+            self.confusionmatrix[facit] = {}
+            self.confusionmatrix[facit][predicted] = 1
+
+    def evaluate(self):
+        for gold in sorted(self.confusionmatrix):
+            print("---")
+            carat = 0
+            maximum = 0
+            hitn = 0
+            sortedglitter = sorted(
+                self.confusionmatrix[gold].items(),
+                key=lambda glitter: glitter[1],
+                reverse=True)
+            for glitter in sortedglitter:
+                hit = ""
+                carat += glitter[1]
+                if glitter[0] == gold:
+                    hit = "***"
+                    hitn = glitter[1]
+                print(gold, glitter[0], glitter[1], hit, sep="\t")
+            print(gold, "sum", hitn, carat, hitn / carat, sep="\t")
+
--- logger.py
+++ logger.py
@@ -1,1 +1,6 @@
-../utils/logger.py
+import inspect
+import time
+
+def logger(msg, level=False, endspace="\n"):
+    if level:
+        print(time.ctime(), " ", inspect.stack()[1][3], "(): ", msg, sep="", end=endspace, flush=True)
