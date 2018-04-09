63de9747c7b3d96581f807ca1ad1f43e3ea03e72
Jussi <jussi@lingvi.st>
2018-04-09 16:38:58 -0700

    properties file handling

---
+++ propertyreader.py
@@ -0,0 +1,24 @@
+# found on stackoverflow, written by Roberto, thx a mil
+
+# The code parses correctly lines like:
+#  url = "http://my-host.com"
+#  name = Paul = Pablo
+#  # This comment line will be ignored
+# You'll get a dict with:
+# {"url": "http://my-host.com", "name": "Paul = Pablo" }
+
+
+def load_properties(filepath, sep='=', comment_char='#'):
+    """
+    Read the file passed as parameter as a properties file.
+    """
+    props = {}
+    with open(filepath, "rt") as f:
+        for line in f:
+            ll = line.strip()
+            if ll and not ll.startswith(comment_char):
+                key_value = ll.split(sep)
+                key = key_value[0].strip()
+                value = sep.join(key_value[1:]).strip().strip('"')
+                props[key] = value
+    return props
