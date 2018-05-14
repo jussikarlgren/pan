5eabe28e20e2171378e1f3322ab2d9040b47c155
Jussi <jussi@lingvi.st>
2018-05-14 11:21:05 -0700

    better output
    feature filtering
    
    confusion matrix for testing

--- 5gramtweetclassifier.py
+++ 5gramtweetclassifier.py
@@ -3,6 +3,9 @@ import re
 import sys
 import pickle
 import xml.etree.ElementTree
+import confusionmatrix
+
+os.environ["CORENLP_HOME"] = "/usr/share/stanford-corenlp-full/"
 
 from propertyreader import load_properties
 
@@ -38,27 +41,30 @@ frequencyweighting = bool(strtobool(properties["frequencyweighting"]))
 fulltext = bool(strtobool(properties["fulltext"]))
 generalise = bool(strtobool(properties["generalise"]))
 featurise = bool(strtobool(properties["featurise"]))
-parse = bool(strtobool(properties["parse"]))
 postriples = bool(strtobool(properties["postriples"]))
 postriplefile = str(properties["postriplefile"])
+language = str(properties["language"])
 outputdirectory = str(properties["outputdirectory"])
 testbatchsize = int(properties["testbatchsize"])
 window = int(properties["window"])
 
-os.environ["CORENLP_HOME"] = corenlphome
+#os.environ["CORENLP_HOME"] = corenlphome
 
 
 stringspace = stringsequencespace.StringSequenceSpace(dimensionality, denseness, window)
 stringspace.importelementspace(charactervectorspacefilename)
+stringspace.importpospermutations(postriplefile)
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
-        resourcedirectory = sys.argv[1]
+        resourcedirectory = sys.argv[1] + "/" + language + "/text/"
     if len(sys.argv) > 2:
-        outputdirectory = sys.argv[2]
+        outputdirectory = sys.argv[2] + "/" + language + "/"
 
-
-
+logger("input from: " + resourcedirectory, monitor)
+logger("output to: " + outputdirectory, monitor)
+if not os.path.exists(outputdirectory):
+    os.makedirs(outputdirectory)
 
 # create new vectors for test set
 
@@ -75,6 +81,7 @@ logger("Start building vectors for " + str(len(filenamelist)) + " test files.", 
 authorindex = 0
 testvectors = {}
 testitemspace = SemanticSpace()
+decentfeatures = squintinglinguist.mediocremale + squintinglinguist.goodgenderones + squintinglinguist.mediocrefemale
 nn = 0
 for file in filenamelist:
     authorname = file.split(".")[0].split("/")[-1]
@@ -82,7 +89,7 @@ for file in filenamelist:
     logger("Reading " + str(authorindex) + " " + file, monitor)
     workingvector = sparsevectors.newemptyvector(dimensionality)
     e = xml.etree.ElementTree.parse(file).getroot()
-
+    hit = False
     for b in e.iter("document"):
         origtext = b.text
         avector = sparsevectors.newemptyvector(dimensionality)
@@ -96,18 +103,22 @@ for file in filenamelist:
         if featurise:
             features = squintinglinguist.featurise(origtext)
             for feature in features:
-                fv = stringspace.getvector(feature)
-                avector = sparsevectors.sparseadd(avector, sparsevectors.normalise(fv),
-                                                  stringspace.frequencyweight(feature))
+                if feature in decentfeatures:
+                    hit = True
+                    fv = stringspace.getvector(feature)
+                    avector = sparsevectors.sparseadd(avector, sparsevectors.normalise(fv),
+                                              stringspace.frequencyweight(feature))
+
         if postriples:
             posttriplevector = stringspace.postriplevector(origtext)
             avector = sparsevectors.sparseadd(avector, sparsevectors.normalise(posttriplevector))
         workingvector = sparsevectors.sparseadd(workingvector, sparsevectors.normalise(avector))
     nn += 1
+    if not hit:
+        logger(authorname + " did not get representation!", error)
     testitemspace.additem(authorindex, workingvector)
     testitemspace.name[authorindex] = authorname
 logger("Done building " + str(nn) + " vectors.", monitor)
-
 
 categories = set()
 targetspace = SemanticSpace()
@@ -115,6 +126,7 @@ logger("Reading from " + categorymodelfilename, monitor)
 cannedindexvectors = open(categorymodelfilename, "rb")
 goingalong = True
 n = 0
+facit = {}
 while goingalong:
     try:
         itemj = pickle.load(cannedindexvectors)
@@ -122,9 +134,9 @@ while goingalong:
         targetspace.category[itemj["authorindex"]] = itemj["category"]
         targetspace.name[itemj["authorindex"]] = itemj["authorname"]
         categories.add(itemj["category"])
+        if itemj["authorname"] in testitemspace.name.values():
+            facit[itemj["authorname"]] = itemj["category"]
         n += 1
-        if n >= testbatchsize:
-            break
     except EOFError:
         goingalong = False
 logger("Read " + str(n) + " target vectors.", monitor)
@@ -137,6 +149,7 @@ if averagelinkage:
 if votelinkage:
     logger("Votelinkage", monitor)
 
+confusionmatrix = confusionmatrix.ConfusionMatrix()
 nn = 0
 for item in testitemspace.items():
     nn += 1
@@ -163,13 +176,14 @@ for item in testitemspace.items():
             targetscore[targetspace.category[neighbour]] += 1
     sortedpredictions = sorted(categories, key=lambda ia: targetscore[ia], reverse=True)
     prediction = sortedpredictions[0]
-    logger("Tested " + testitemspace.name[item] + ": " + prediction, monitor)
+    logger("Tested " + testitemspace.name[item] + ": " + prediction + " " + facit[testitemspace.name[item]], debug)
+    confusionmatrix.addconfusion(facit[testitemspace.name[item]], prediction)
     with open(outputdirectory + testitemspace.name[item] + ".xml", "w") as outfile:
-        print("<author id=" + testitemspace.name[item], file=outfile)
-        print("        lang=en", file=outfile)
-        print("        gender_txt=" + prediction, file=outfile)
+        print("<author id=\"" + testitemspace.name[item] + "\"", file=outfile)
+        print("        lang=\"en\"", file=outfile)
+        print("        gender_txt=\"" + prediction + "\"", file=outfile)
         print("/>", file=outfile)
-
+        logger("Run output to " + outputdirectory + testitemspace.name[item] + ".xml", monitor)
 logger("Done testing files.", monitor)
-
+confusionmatrix.evaluate()
 
