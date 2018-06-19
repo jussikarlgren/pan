0ddc5ac59ef1c79beb1767d40e6d6ec7c2288309
Jussi <jussi@lingvi.st>
2018-06-19 04:51:22 -0700

    xval cycles; hack to handle corenlp env variable

--- 5gramtweettester.py
+++ 5gramtweettester.py
@@ -2,6 +2,8 @@ import os
 import re
 import xml
 
+os.environ["CORENLP_HOME"] = "/usr/share/stanford-corenlp-full/"
+
 import sparsevectors
 import squintinglinguist
 import stringsequencespace
@@ -19,6 +21,7 @@ visavis the precomputed vectors. Properties in separate file.
 """
 
 properties = load_properties("5gramtweettester.properties")
+corenlp = properties["CORENLP_HOME"]
 debug = bool(strtobool(properties["debug"]))
 monitor = bool(strtobool(properties["monitor"]))
 error = bool(strtobool(properties["error"]))
@@ -40,10 +43,12 @@ frequencyweighting = bool(strtobool(properties["frequencyweighting"]))
 fulltext = bool(strtobool(properties["fulltext"]))
 generalise = bool(strtobool(properties["generalise"]))
 featurise = bool(strtobool(properties["featurise"]))
-parse = bool(strtobool(properties["parse"]))
 postriples = bool(strtobool(properties["postriples"]))
 postriplefile = str(properties["postriplefile"])
 window = int(properties["window"])
+cycles = int(properties["cycles"])
+unknownbatchsize = int(properties["unknownbatchsize"])
+
 
 stringspace = stringsequencespace.StringSequenceSpace(dimensionality, denseness, window)
 stringspace.importelementspace(charactervectorspacefilename)
@@ -134,71 +139,110 @@ for file in testfiles:
 logger("Done building " + str(nn) + " vectors.", monitor)
 
 
-resultaggregator = []
-items = list(targetspace.items())
-logger("Calculating neighbours for " + str(len(testitemspace.items())) +
-       " test items and " + str(len(targetspace.items())) + " target items.", monitor)
+for cycle in range(cycles):
+    testitemnames = list(testitemspace.items())
+    random.shuffle(testitemnames)
+    testers = testitemnames[:unknownbatchsize]
 
-neighbours = {}
-for item in testitemspace.items():
-    neighbours[item] = {}
-    for otheritem in targetspace.items():
-        if testitemspace.name[item] == targetspace.name[otheritem]:
-            continue
-        neighbours[item][otheritem] = sparsevectors.sparsecosine(testitemspace.indexspace[item], targetspace.indexspace[otheritem])
-logger("Done calculating neighbours", monitor)
+    logger("Cycle " + str(cycle) + " of " + str(cycles) + "tests.")
+    items = list(targetspace.items())
+    logger("Calculating neighbours for " + str(len(testitemspace.items())) +
+           " test items and " + str(len(targetspace.items())) + " target items.", monitor)
 
-result = {}
-for c in categories:
-    result[c] = {}
+    neighbours = {}
+    for item in testers:
+        neighbours[item] = {}
+        for otheritem in targetspace.items():
+            if testitemspace.name[item] == targetspace.name[otheritem]:
+                continue
+            neighbours[item][otheritem] = sparsevectors.sparsecosine(testitemspace.indexspace[item], targetspace.indexspace[otheritem])
+    logger("Done calculating neighbours", monitor)
 
-logger("Pool depth " + str(itempooldepth), monitor)
-if averagelinkage:
-    logger("Averagelinkage", monitor)
-if votelinkage:
-    logger("Votelinkage", monitor)
-confusion = ConfusionMatrix()
-primeconfusion = ConfusionMatrix()
-targetscore = {}
-for item in testitemspace.items():
-    sortedneighbours = sorted(neighbours[item], key=lambda hh: neighbours[item][hh], reverse=True)[:itempooldepth]
-    primeconfusion.addconfusion(facittable[testitemspace.name[item]], targetspace.category[sortedneighbours[0]])
-    for target in categories:
-        targetscore[target] = 0
-    if averagelinkage:  # take all test neighbours and sum their scores
-        for neighbour in sortedneighbours:
-            targetscore[targetspace.category[neighbour]] += neighbours[item][neighbour]
-    elif votelinkage:
-        for neighbour in sortedneighbours:
-            targetscore[targetspace.category[neighbour]] += 1
-    sortedpredictions = sorted(categories, key=lambda ia: targetscore[ia], reverse=True)
-    prediction = sortedpredictions[0]
-    logger(prediction + "?" + " " + facittable[testitemspace.name[item]] + ".", debug)
-    for iii in range(itempooldepth):
+
+    logger("Pool depth " + str(itempooldepth), monitor)
+    if averagelinkage:
+        logger("Averagelinkage", monitor)
+    if votelinkage:
+        logger("Votelinkage", monitor)
+    confusion = ConfusionMatrix()
+    primeconfusion = ConfusionMatrix()
+    targetscore = {}
+    for item in testers:
+        sortedneighbours = sorted(neighbours[item], key=lambda hh: neighbours[item][hh], reverse=True)[:itempooldepth]
+        primeconfusion.addconfusion(facittable[testitemspace.name[item]], targetspace.category[sortedneighbours[0]])
+        for target in categories:
+            targetscore[target] = 0
+        if averagelinkage:  # take all test neighbours and sum their scores
+            for neighbour in sortedneighbours:
+                targetscore[targetspace.category[neighbour]] += neighbours[item][neighbour]
+        elif votelinkage:
+            for neighbour in sortedneighbours:
+                targetscore[targetspace.category[neighbour]] += 1
+        sortedpredictions = sorted(categories, key=lambda ia: targetscore[ia], reverse=True)
+        prediction = sortedpredictions[0]
+        logger(prediction + "?" + " " + facittable[testitemspace.name[item]] + ".", debug)
+        for iii in range(itempooldepth):
+            try:
+                logger(testitemspace.name[item] + " (" + facittable[testitemspace.name[item]] + ") " +
+                      "\t" + str(neighbours[item][sortedneighbours[iii]]) + "\t" + targetspace.name[sortedneighbours[iii]] +
+                      " (" + targetspace.category[sortedneighbours[iii]] + ") "
+                      , debug)
+            except:
+                logger("keyerror " + str(iii), error)
+        confusion.addconfusion(facittable[testitemspace.name[item]], prediction)
+    print("1","-----------")
+    primeconfusion.evaluate()
+    print(itempooldepth,"-----------")
+    confusion.evaluate()
+    recallresult = {}
+    precisionresult = {}
+    precisionresult["both"] = {}
+    correct = {}
+    correct[itempooldepth] = 0
+    correct[1] = 0
+    cands = {}
+    cands[itempooldepth] = 0
+    cands[1] = 0
+    for c in categories:
+        recallresult[c] = {}
+        precisionresult[c] = {}
         try:
-            logger(testitemspace.name[item] + " (" + facittable[testitemspace.name[item]] + ") " +
-                  "\t" + str(neighbours[item][sortedneighbours[iii]]) + "\t" + targetspace.name[sortedneighbours[iii]] +
-                  " (" + targetspace.category[sortedneighbours[iii]] + ") "
-                  , debug)
-        except:
-            logger("keyerror " + str(iii), error)
-    confusion.addconfusion(facittable[testitemspace.name[item]], prediction)
-confusion.evaluate()
-primeconfusion.evaluate()
-for c in categories:
-    try:
-        result[c][itempooldepth] = confusion.carat[c] / confusion.weight[c]
-    except KeyError:
-        result[c][itempooldepth] = 0
-    try:
-        result[c][1] = primeconfusion.carat[c] / primeconfusion.weight[c]
-    except KeyError:
-        result[c][1] = 0
-logger("Done testing.", monitor)
-for c in categories:
-    print(c, result[c], sep="\t")
-    resultaggregator.append((c, result[c]))
-print(str(resultaggregator))
-
-# for adf in neighbours[1]:
-#    print(adf, neighbours[1][adf], targetspace.name[adf])
+            cands[itempooldepth] += confusion.glitterweight[c]
+            correct[itempooldepth] += confusion.carat[c]
+            recallresult[c][itempooldepth] = confusion.carat[c] / confusion.weight[c]
+            precisionresult[c][itempooldepth] = confusion.carat[c] / confusion.glitterweight[c]
+        except KeyError:
+            recallresult[c][itempooldepth] = 0
+        try:
+            cands[1] += primeconfusion.glitterweight[c]
+            correct[1] += primeconfusion.carat[c]
+            recallresult[c][1] = primeconfusion.carat[c] / primeconfusion.weight[c]
+            precisionresult[c][1] = primeconfusion.carat[c] / primeconfusion.glitterweight[c]
+        except KeyError:
+            recallresult[c][1] = 0
+    precisionresult["both"][itempooldepth] = correct[itempooldepth] / cands[itempooldepth]
+    precisionresult["both"][1] = correct[1] / cands[1]
+    logger("Done testing.", monitor)
+    outputcategories = list(categories) + ["both"]
+    print("Precision")
+    for c in outputcategories:
+        print(" ", "&", c, sep="\t", end="\t")
+    for c in outputcategories:
+        print(" ", "&", c, sep="\t", end="\t")
+    print()
+    for c in outputcategories:
+        print(" ", "&", precisionresult[c][1], sep="\t", end="\t")
+    for c in outputcategories:
+        print(" ", "&", precisionresult[c][itempooldepth], sep="\t", end="\t")
+    print()
+    print("Recall")
+    for c in categories:
+        print(" ", "&", c, sep="\t", end="\t")
+    for c in categories:
+        print(" ", "&", c, sep="\t", end="\t")
+    print()
+    for c in categories:
+        print(" ", "&", recallresult[c][1], sep="\t", end="\t")
+    for c in categories:
+        print(" ", "&", recallresult[c][itempooldepth], sep="\t", end="\t")
+    print()
